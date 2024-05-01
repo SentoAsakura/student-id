59cf5f0e565cf245505eac02058c23112bb067ca
TThien08 <thehientv0609@gmail.com>
2024-05-01 14:24:50 +0700

    warcime commited

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import cv2
 from pyzbar.pyzbar import decode
 import numpy as np
 
+fo = open('shit.txt','a')
+
 cam = cv2.VideoCapture(0)
 while True:
     ret,frame = cam.read()
@@ -9,15 +11,16 @@ while True:
         pts = np.array([code.polygon],np.int32)
         pts = pts.reshape((-1,1,2))
         cv2.polylines(frame,[pts],True,(0,255,255),3)
+        #print(code)
     
     cv2.imshow('frame',frame)
 
     key = cv2.waitKey(1)
     
-    if key == 'a':
+    if key == 13:
         data=code.data.decode('utf-8')
         print('A:',data)
-    if key==ord('q'):
+    if key==113:
         break
 
 cam.release()
