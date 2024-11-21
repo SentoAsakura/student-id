80135037083b6ac8a222de3cd27aaf9539e4c480
Sento Asakura <zenohakai@gmail.com>
2024-11-21 14:52:57 +0700

    Idk, I'm cooking

--- check.py
+++ check.py
@@ -1,14 +1,22 @@
 import time
+import json
+setting = json.load(open('setting.json'))
 
+mode1 = setting["mode1"]
+mode2 = setting["mode2"]
 class logTime():
     def __init__(self):
         self.time = time.localtime()
         self.hour = self.time.tm_hour
         self.min = self.time.tm_min
-        self.state = self.check()
-    def check(self):
-        if self.hour>=7:
-            return 'Trễ'
-        if self.hour == 6 and self.min>45:
-            return 'Trễ'
-        else: return 'Đúng giờ'
+        self.state = self.check(setting["mode"])
+    def check(self, mode):
+        match mode:
+            case '1':
+                if self.hour >= mode1[0] and self.min>mode1[1]:
+                    return 'Trễ'
+                else: return 'Đúng giờ'
+            case '2':
+                if self.hour >= mode2[0] and self.min>mode2[1]:
+                    return 'Trễ'
+                else: return 'Đúng giờ'
